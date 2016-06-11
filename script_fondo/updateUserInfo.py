497d2896239ae1813d4fa13e2075490c1a19e624
minmiguelm <cmiguelmg@gmail.com>
2016-06-11 16:26:01 -0500

    some code

---
+++ script_fondo/updateUserInfo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import json
+import requests
+import sys
+
+load = 0
+inf_users = []
+domain="http://192.168.0.15:8181/"
+
+def loadFile():
+    f = open(sys.argv[1],"r")
+    for line in f:
+        data = line.strip().split("\t")
+        id = data[0].split()[0]
+        balance = data[1].split()[1]
+        quota = data[2].split()[1]
+        contributions = data[3].split()[1]
+        valueCredits = data[4].split()[1]
+        inf_users.append([id,balance,quota,contributions,valueCredits])
+    global load
+    load = 1
+    f.close()
+
+def amountCredits(id):
+    r = requests.get(domain + "requests/getByIdMemberState/%i" % id)
+    amount = r.text
+    return amount
+
+def updateUser(userInfo):
+    id = int(userInfo[0])
+    r = requests.get(domain + "members/getByIdMember/%i" % (id))
+    body = json.loads(r.text)
+    body['balance'] = userInfo[1]
+    body['quota'] = userInfo[2]
+    body['contributions'] = userInfo[3]
+    body['amountCredits'] = amountCredits(id)
+    body['valueCredits'] = userInfo[4]
+    requests.post(domain + "members/update",data=json.dumps(body),headers={"content-type": "application/json"})
+
+loadFile()
+if load == 1:
+    for i in inf_users:
+        updateUser(i)
